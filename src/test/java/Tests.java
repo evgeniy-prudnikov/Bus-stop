715b41ce557d18f743ada8d6ffaa3dc71c69330f
jenechkaaaaaaaaaaa <genya5894@gmail.com>
2021-03-29 23:22:08 +0300

    Added fixes and tests

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -11,22 +11,21 @@ public class Tests {
     @Test
     public void test() throws Exception {
 
-        FileOutputStream fileOutputStream = new FileOutputStream("input.txt");
-        Random random = new Random();
-        while (random.nextInt() % 10 != 7) {
+        try (FileOutputStream fileOutputStream = new FileOutputStream("input.txt") ) {
+            Random random = new Random();
+            while (random.nextInt() % 10 != 7) {
 
-            long a = Math.abs(random.nextLong() % 86400000);
-            long b = Math.abs(random.nextLong() % 60 * 60 * 1000);
-            b = a + b;
+                long a = Math.abs(random.nextLong() % 86400000);
+                long b = Math.abs(random.nextLong() % 60 * 60 * 1000);
+                b = a + b;
 
-            String str = company[Math.abs(random.nextInt() % 2)] + " " + Service.showTime(a) +
-                    " " + Service.showTime(b) + "\n";
+                String str = company[Math.abs(random.nextInt() % 2)] + " " + Service.showTime(a) +
+                        " " + Service.showTime(b) + "\n";
 
-            fileOutputStream.write(str.getBytes());
+                fileOutputStream.write(str.getBytes());
+            }
+            fileOutputStream.flush();
         }
-        fileOutputStream.flush();
-        fileOutputStream.close();
-
 
         Main.main(new String[]{"input.txt"});
 
